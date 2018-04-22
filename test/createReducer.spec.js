3dba9680b302ee3240ee7b9c489873f31bb0c045
georapbox <georapbox@gmail.com>
2018-04-22 10:01:43 +0300

    test placeholder

---
+++ test/createReducer.spec.js
@@ -0,0 +1,11 @@
+// import createReducer from '../src';
+import chai from 'chai';
+
+const { expect } = chai;
+
+describe('createReducer', () => {
+  it('should pass', () => {
+    const foo = 1;
+    expect(foo).to.eql(1);
+  });
+});
